8e9995215cdd5c749ea2e7fc7d684d058d1fcc4a
VictorBjelkholm <victorbjelkholm@gmail.com>
2012-10-27 17:38:11 +0200

    Ändrat " till ' och lagt till nya ikoner

--- background.js
+++ background.js
@@ -1,22 +1,22 @@
 /*global chrome: false, localStorage: false, console: false,*/
 //Detta är kod som körs hela tiden. Sätt loop för notifikationer och
-//också en listener för att ta emot meddelanden från content.js 
+//också en listener för att ta emot meddelanden från content.js
 
 var storage, i;
 
 chrome.extension.onRequest.addListener(function (data, sender, sendResponse) {
 	'use strict';
-	if (data.method === "getSettings") {
+	if (data.method === 'getSettings') {
 		sendResponse(localStorage);
 	}
-	if (data.method === "addHighlight") {
+	if (data.method === 'addHighlight') {
 		console.log('addHighlight');
 		storage = JSON.parse(localStorage.getItem('highlightWords'));
 		storage.push({'word': data.word, 'color': data.color});
 		storage = JSON.stringify(storage);
 		localStorage.setItem('highlightWords', storage);
 	}
-	if (data.method === "removeHighlight") {
+	if (data.method === 'removeHighlight') {
 		console.log('removeHighlight');
 		storage = JSON.parse(localStorage.getItem('highlightWords'));
 
@@ -34,13 +34,12 @@ chrome.extension.onRequest.addListener(function (data, sender, sendResponse) {
 		storage = JSON.stringify(storage);
 		localStorage.setItem('highlightWords', storage);
 	}
-	if (data.method === "saveSetting") {
+	if (data.method === 'saveSetting') {
 		localStorage.setItem(data.data.id, data.data.value);
 		console.log(data.data.id + ' = ' + data.data.value);
 	}
-	if (data.method === "getNotifications") {
+	if (data.method === 'getNotifications') {
 		sendResponse(localStorage.getItem('notifications'));
 	}
 });
-
 // TODO Lägga till funktioner för att kolla efter och lägga till notifikationer
